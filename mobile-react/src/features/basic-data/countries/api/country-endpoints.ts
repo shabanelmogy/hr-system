ca38f0ec91fd469afdd14571d761cc26d9a0fce3
user <user@WIN-ST2GVRTI94F>
2026-09-12 10:00:21 +0300

    update

--- mobile-react/src/features/basic-data/countries/api/country-endpoints.ts
+++
@@ -1,9 +0,0 @@
-export const countryEndpoints = {
-  base: 'countries',
-  lookup: 'countries/lookup',
-  byId: (id: number) => `countries/${id}`,
-  withStates: (id: number) => `countries/${id}/states`,
-  restore: (id: number) => `countries/${id}/restore`,
-  bulkArchive: 'countries/bulk-archive',
-  bulkCreate: 'countries/bulk',
-} as const;
--- mobile-react/src/features/basic-data/countries/queries/country-keys.ts
+++
@@ -1,9 +0,0 @@
-import type { CountryPageQuery } from '../types/country';
-
-export const countryKeys = {
-  all: ['countries'] as const,
-  lists: () => [...countryKeys.all, 'list'] as const,
-  list: (query: CountryPageQuery) => [...countryKeys.lists(), query] as const,
-  lookup: () => [...countryKeys.all, 'lookup'] as const,
-  reportCatalog: () => [...countryKeys.all, 'reports', 'catalog'] as const,
-};
